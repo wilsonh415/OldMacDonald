6c2bb9cd31594bea3940f6f4f08bf9b5125d48b7
wihuang10 <wilsonh415@gmail.com>
2016-10-14 10:45:35 -0700

    Still need to name cow

--- Chick.java
+++ Chick.java
@@ -1,4 +1,25 @@
-class Chick //implements Animal 
+class Chick implements Animal 
 {     
-     //your code here
+    private String myType;     
+     private String mySound;   
+     private String mySound2;   
+     public Chick(String type, String sound, String sound2)     {         
+         myType = type;         
+         mySound = sound;
+         mySound2 = sound2;     
+     }     
+     public Chick()     {         
+         myType = "unknown";         
+         mySound = "unknown"; 
+         mySound2 = "unknown";    
+     }      
+     public String getSound() {
+     	if(Math.random() > .5 ) {
+     	return mySound;
+     }
+    else {
+     	return mySound2;
+     }
+     }   
+     public String getType(){return myType;} 
 }
--- OldMacDonald.pde
+++ OldMacDonald.pde
@@ -1,6 +1,10 @@
-
-void setup()
-{
-	//your variable declarations here
+public void setup() {     
+    /* Cow c = new Cow("cow", "moo");  { 
+    System.out.println(c.getType() + " goes " + c.getSound());  }  
+    Chick k = new Chick("chick","cluck","cheep"); {
+    System.out.println(k.getType() + " goes " + k.getSound()); }
+    Pig p = new Pig("pig","oink"); {
+    System.out.println(p.getType() + " goes " + p.getSound()); } */
+    Farm x = new Farm();
+    x.animalSounds();
 }
-
